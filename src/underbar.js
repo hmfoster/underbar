29c634206b25962d47d70629eb8a48b38d999699
Hailey Foster <haileyhasmail@gmail.com>
2015-05-05 18:08:20 -0700

    Clean up _.extend.

--- src/underbar.js
+++ src/underbar.js
@@ -232,17 +232,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-    if (arguments.length === 1){
-      return obj;
-    }
-    else {
     _.each(arguments, function(item){
       _.each(item, function(value, key){
         obj[key] = value;
       });
    });
      return obj;
-   };
   };
 
   // Like extend, but doesn't ever overwrite a key that already
