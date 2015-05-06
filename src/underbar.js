e67f3cc319139005ac7e22ac1fc1e631188096ca
Hailey Foster <haileyhasmail@gmail.com>
2015-05-05 18:06:02 -0700

    Modify _.extend to pass tests.

--- src/underbar.js
+++ src/underbar.js
@@ -210,7 +210,6 @@
       };
       return Boolean(iterator(item));
     }, false)
-
   };
 
 
@@ -233,6 +232,17 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    if (arguments.length === 1){
+      return obj;
+    }
+    else {
+    _.each(arguments, function(item){
+      _.each(item, function(value, key){
+        obj[key] = value;
+      });
+   });
+     return obj;
+   };
   };
 
   // Like extend, but doesn't ever overwrite a key that already
